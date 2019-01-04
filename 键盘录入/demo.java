0b02a6b2145dd1b7daa4cb5060a17d0e03715d62
唐都 <7277710@qq.com>
2019-01-04 14:27:35 +0800

    1.键盘录入

---
+++ 键盘录入/demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class demo{
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("请输入第1整数");
+	int x = sc.nextInt();
+	System.out.println("请输入第2个整数");
+	int y = sc.nextInt();
+	int sum = x + y;
+	System.out.println(sum);
+	}
+}
